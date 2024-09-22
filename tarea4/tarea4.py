eb41473fcd27b43cb2f094d5553d706bcef65a8c
dancaseb <danielisreeves@gmail.com>
2024-09-21 23:14:07 -0600

    add plotting of cars

--- tarea4/tarea4.py
+++ tarea4/tarea4.py
@@ -1,4 +1,6 @@
 import numpy as np
+import matplotlib.pyplot as plt
+
 
 distraction_probability = 0.1
 iterations = 5
@@ -8,10 +10,11 @@ vector_length = 15
 
 
 class Car:
-    def __init__(self, speed, position, distraction_probability):
+    def __init__(self, speed, position, distraction_probability, colour):
         self.speed = speed
         self.position = position
         self.distraction_probability = distraction_probability
+        self.colour = colour
     
     def move(self, car_ahead_position: int):
         # acceleration
@@ -32,24 +35,39 @@ class Car:
 
 class Simulation:
     def __init__(self, iterations, vector_length):
-        self.matrix = np.zeros((iterations, vector_length), dtype=int)
+        self.matrix = np.ones((iterations, vector_length), dtype=int)
+        self.matrix *= -1 # -1 will be empty spot, 0 is a car with speed 0
         random_positions = np.random.choice(vector_length, num_cars, replace=False)
         random_positions.sort()
-        self.cars = [Car(np.random.randint(1, max_speed + 1), random_positions[i], distraction_probability) for i in range(num_cars)]
+        self.cars = [Car(np.random.randint(1, max_speed + 1), random_positions[i], distraction_probability, np.random.uniform(0,0.9,size=3)) for i in range(num_cars)]
         self.matrix[0, [car.position for car in self.cars]] = [car.speed for car in self.cars] # initial state
+        self.grid = np.ones((iterations, vector_length, 3)) # grid for plotting the cars
+        self.grid[0, [car.position for car in self.cars]] = [car.colour for car in self.cars]
+
 
     def simulate(self, step):
         for index, car in enumerate(self.cars):
             car_ahead_position = self.cars[(index + 1) % num_cars].position
             car.move(car_ahead_position)
-        self.matrix[step, [car.position for car in self.cars]] = [car.speed for car in self.cars]
+        self.matrix[step, [car.position for car in self.cars]] = [car.speed for car in self.cars] # update computational matrix
+        self.grid[step, [car.position for car in self.cars]] = [car.colour for car in self.cars] # update plotting matrix
+    
+    def plot_simulation(self):
+        fig, ax = plt.subplots()
+        ax.imshow(self.grid)
+        for i, row in enumerate(self.matrix):
+            for j, element in enumerate(row):
+                if element != -1:
+                    ax.text(j, i, f"{element}", va='center', ha='center', fontsize=12, color='black')
+        ax.set_xticks(np.arange(vector_length + 1) - 0.5, minor=True)
+        ax.set_yticks(np.arange(iterations + 1) - 0.5, minor=True)
+        ax.grid(which="minor", color="black", linestyle='-', linewidth=1)
 
-        
+        plt.show()
 
-
-# 
+ 
 s = Simulation(iterations, vector_length)
 for step in range(1, iterations):
     s.simulate(step)
 print(s.matrix)
-# print (simulation)
+s.plot_simulation()
